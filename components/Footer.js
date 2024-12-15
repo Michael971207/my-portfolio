c8d02120a468a803a51bfe39420bd876d691299c
Michael Moulay <mich@Macmini.home>
2024-12-15 12:56:51 +0100

    Updated site structure and added all sections

--- components/Footer.js
+++ components/Footer.js
@@ -1,8 +1,7 @@
 export default function Footer() {
-    return (
-      <footer className="bg-gray-800 text-white p-4 text-center">
-        <p>© 2024 Michael&apos;s Portfolio. All rights reserved.</p>
-      </footer>
-    );
-  }
-  
+  return (
+    <footer className="bg-gray-800 text-white p-4 text-center">
+      <p>© 2024 Michael&apos;s Portfolio. All rights reserved.</p>
+    </footer>
+  );
+}
